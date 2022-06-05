65ad6ab1b58781665efe7d0e6c314cb1930c78b2
Alice S <snapp@null.net>
2022-06-05 10:46:30 -0400

    Add minefield iteration blooming and numbers!
    Minesweeper is a lot deeper than one would imagine

--- main.py
+++ main.py
@@ -48,7 +48,7 @@ class Game:
 		for i in range(_mines):
 			x = r.randint(0, dim-1)
 			y = r.randint(0, dim-1)
-			_field[x][y] = Minefield(self, True)
+			_field[y][x] = Minefield(self, True)
 		print('Mines planted.')
 		self._field = _field
 
@@ -169,6 +169,12 @@ class Minefield:
 		self._ismine = ismine             # is this Minefield a mine?
 		self._game = Game
 		self._state = self.states.fresh   # a new minefield shall always be fresh
+		self._x = 0
+		self._y = 0
+		self._button = Button(tk,
+			padx=10,
+			pady=3)
+		self._button.configure(command=lambda: self.onclick(self._button))
 
 	"""
 	Properties
@@ -180,6 +186,23 @@ class Minefield:
 	@property
 	def state(self):
 		return self._state
+
+	@property  # getter for X value
+	def x(self):
+		return self._x
+
+	@property
+	def y(self):
+		return self._y
+	
+	@property
+	def cord(self):
+		return (self.x, self.y)
+
+	@property
+	def button(self):
+		return self._button
+	
 	
 	"""
 	Setters
@@ -191,18 +214,38 @@ class Minefield:
 	@state.setter
 	def state(self, s):
 		try:
-			if type(s) is str:
-				s = self.states[s]
+			if type(s) is not str:
+				s = s.name
 			if s in self.states.__members__:
-				self._state = s
+				button = self._button
+				match s:
+					case 'dug':
+						button['relief'] = SUNKEN
+					case 'flagged':
+						button['text'] = 'P'
+				print('Minefield has been ' + s)
+				self._state = self.states[s]
 		except:
 			raise ValueError("Invalid Minefield state.")
+
+	@x.setter
+	def x(self, value):
+		self._x = value
+
+	@y.setter
+	def y(self, value):
+		self._y = value
+
+	@cord.setter
+	def cord(self, value):
+		self.x, self.y = value
 
 	"""
 	Methods
 	"""
-	def onclick(self, Game):
-		mouse = Game.player.mouse
+	def onclick(self, button):
+		# change the state of the minefield and update the button
+		mouse = self._game.player.mouse
 		if mouse.__class__ is not Mouse:
 			pass
 		match mouse.state.name:
@@ -215,10 +258,9 @@ class Minefield:
 					else:
 						print('The player is safe... for now.')
 					self.state = 'dug'
-					self.render()
+					self.bloom()
 				else:
 					print('The Player cannot dig here.')
-
 			case 'flag':
 				# the field is flagged
 				if self.state.value > 1:
@@ -226,24 +268,69 @@ class Minefield:
 					print("The Player cannot place a flag here.")
 				else:
 					self.state = 'flagged'
-					self.render()
 
+	def bloom(self):
+		# branch out and find mines
+		gamefield = self._game.field
+		x, y = self.cord
+		go = dict(
+			up        = lambda n=1, nx=x, ny=y: gamefield[ny - n][nx + 0],
+			right     = lambda n=1, nx=x, ny=y: gamefield[ny + 0][nx + n], 
+			down      = lambda n=1, nx=x, ny=y: gamefield[ny + n][nx + 0],
+			left      = lambda n=1, nx=x, ny=y: gamefield[ny + 0][nx - n],
 
-	def render(self):
-		field = Button(tk,
-			padx=10,
-			pady=3,
-			command=lambda: self.onclick(self._game))
+			# used for check()
+			upright   = lambda n=1, nx=x, ny=y: gamefield[ny - n][nx + n],
+			upleft    = lambda n=1, nx=x, ny=y: gamefield[ny - n][nx - n],
+			downright = lambda n=1, nx=x, ny=y: gamefield[ny + n][nx + n],
+			downleft  = lambda n=1, nx=x, ny=y: gamefield[ny + n][nx - n],)
 
-		if self.state.name == 'dug':
-			field.config(relief=SUNKEN)
-		else:
-			field.config(relief=RAISED)
-		if self.state.name == 'flagged':
-			field.config(text='F')
+		def check(obj=self, directions=[]):
+			# check how many mines are nearby
+			_directions = directions
+			_found = []
+			gox, goy = obj.cord
+			if len(_directions) == 0:
+				# creates a default list based on fresh, untouched minefields
+				for i, v in list(go.items()):
+					if v().state.value <= 1:
+						_directions.append(i)
+			for there in _directions:
+				if go[there](nx = gox, ny = goy).ismine:
+					_found.append(there)
+			print(_found)
+			return _found
 
-		#field.command = 
-		return field # a Tkinter Button
+		def branch(direction):
+			# nested method to yield minefields until finding a mine
+			print('branching ' + direction)
+			index = 1
+			fields = []
+			try:
+				while True:
+					print(f'[{index}] Branching {direction}.')
+					yield go[direction](index)
+					index += 1
+			except Exception as e:
+				print(e)
+
+		inicheck = check() # first make sure there are no mines nearby
+		checked = dict()
+		go_cardinal = list(go.items())[0:4]
+		go_fan = iter(go_cardinal)
+		for direction, thisfield in go_cardinal:
+			if direction in inicheck:
+				continue  # there's already a mine here so there's no need to check
+			for i in branch(direction):
+				if len(check(i)) > 0:
+					i.button['text'] = len(check(i))
+					break #the next field in this direction is a mine, stop here
+				i.state = 'dug'
+				for e in branch(next(go_fan)[0]):
+					if len(check(e)) > 0:
+						e.button['text'] = len(check(e))
+						break
+					e.state = 'dug'
 
 class Window:
 	"""
@@ -257,10 +344,13 @@ class Window:
 	def render_field(self):
 		print('rendering buttons')
 		_field = self._game.field
-		for x, field_list in enumerate(_field):
-			print(x)
-			for y, field_obj in enumerate(field_list):
-				field_obj.render().grid(row=x,column=y)
+		field_list_simple = list()
+		for y, field_list in enumerate(_field):
+			for x, field_obj in enumerate(field_list):
+				field_obj.button.grid(row=y,column=x)
+				field_obj.cord = (x, y)
+				#field_obj.button['text'] = f'{x}, {y}' # debug
+		return field_list_simple
 
 Window(Game())
 
